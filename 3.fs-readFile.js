c0c39d95237cf56757c502a16561576313eaa1fb
Juan <juagosin@gmail.com>
2024-01-07 22:33:35 +0100

    Asincronos: Callbacks

--- 3.fs-readFile.js
+++ 3.fs-readFile.js
@@ -1,13 +1,14 @@
 const fs = require('node:fs')
 
 console.log('Leyendo el primer archivo')
-const text = fs.readFileSync('./archivo.txt', 'utf-8')
+fs.readFile('./archivo.txt', 'utf-8', (err, text)=>{ //ejecuta el callback
+    console.log('primer texto:', text)
+})
 
-console.log(text)
-
-console.log('Hacer cosas mientras lee el archivo...')
+console.log('-->Hacer cosas mientras lee el archivo...')
 
 console.log('Leyendo el segundo archivo')
-const secondText = fs.readFileSync('./archivo2.txt', 'utf-8')
+fs.readFile('./archivo2.txt', 'utf-8', (err, text)=>{ //ejecuta el callback
+    console.log('segundo texto: ', text)
+})
 
-console.log(secondText)
