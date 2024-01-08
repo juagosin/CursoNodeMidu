844354b0f5563dfe32212bec54bf55f3ad2929fe
Juan <juagosin@gmail.com>
2024-01-08 22:38:47 +0100

    Objeto global process

---
+++ 7.process.js
@@ -0,0 +1,14 @@
+//argumentos de entrada
+console.log(process.argv)
+
+
+//controlar el proceso y su salida
+//process.exit(1)//0 todo ok, 1
+
+//podemos controlar eventos del proceso
+process.on('exit',()=>{
+    //limpiar la consola  o recursos
+})
+
+//current working directory
+console.log(process.cwd())
