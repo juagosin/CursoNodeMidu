457e20ebcaae38965fd51e385fc7d5d6fb5a1b8d
Juan <juagosin@gmail.com>
2024-01-08 22:26:51 +0100

    Base, nombre archivo y extension

--- 5.path.js
+++ 5.path.js
@@ -6,3 +6,12 @@ console.log(path.sep)
 //unir rutas con path.join
 const filePath = path.join('/content', 'subfolder', 'text.txt')
 console.log (filePath)
+
+const base = path.basename('temp/juan-secret-files/password.txt')
+console.log(base)
+
+const filename = path.basename('temp/juan-secret-files/password.txt', '.txt')
+console.log(filename)
+
+const extension = path.extname('my.super.image.jpg')
+console.log(extension)
