b346a4ba7bfca7216c7714e6c59e10e11ec7fe76
Juan <juagosin@gmail.com>
2024-01-08 22:33:58 +0100

    ls con promesa

--- 6.ls.js
+++ 6.ls.js
@@ -1,11 +1,14 @@
-const fs = require('node:fs')
+const fs = require('node:fs/promises')
 
-fs.readdir('.',(err, files)=>{
+fs.readdir('.')
+.then(files=>{
+    files.forEach(file=>{
+        console.log(file)
+    })
+})
+.catch(err=>{
     if(err){
         console.log('Error al leer el directorio: ', err)
         return;
     }
-    files.forEach(file=>{
-        console.log(file)
-    })
 })
