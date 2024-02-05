fc73196c171f44d83931163dae65b71ed798c2e6
Juan <jugomez@howdeniberia.com>
2024-02-05 15:41:07 +0100

    Use picocolors

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -1,13 +1,14 @@
 const fs = require('node:fs/promises')
 const path = require('node:path')
 const folder = process.argv[2] ?? '.'
+const pc = require('picocolors')
 
 async function ls (directory){
     let files
     try{
         files = await fs.readdir(folder)     
     }catch{
-        console.error(`No se ha podido leer el directorio ${folder}`)
+        console.error(pc.red(`No se ha podido leer el directorio ${folder}`))
         process.exit(1)
     }
 
@@ -27,7 +28,7 @@ async function ls (directory){
         const fileSize = stats.size.toString()
         const fileModified = stats.mtime.toLocaleString()
 
-        return `${fileType} ${file.padEnd(20)} ${fileSize.padStart(10)} ${fileModified}`
+        return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
 
     })
     
