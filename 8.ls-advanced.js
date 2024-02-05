fdfcba1713789066dc80999b7cd74ff402e7b113
Juan <jugomez@howdeniberia.com>
2024-02-05 16:51:06 +0100

    Add Eslint

--- 8.ls-advanced.js
+++ 8.ls-advanced.js
@@ -3,38 +3,36 @@ const path = require('node:path')
 const folder = process.argv[2] ?? '.'
 const pc = require('picocolors')
 
-async function ls (directory){
-    let files
-    try{
-        files = await fs.readdir(folder)     
-    }catch{
-        console.error(pc.red(`No se ha podido leer el directorio ${folder}`))
-        process.exit(1)
+async function ls (directory) {
+  let files
+  try {
+    files = await fs.readdir(folder)
+  } catch {
+    console.error(pc.red(`No se ha podido leer el directorio ${folder}`))
+    process.exit(1)
+  }
+
+  const filesPromises = files.map(async file => {
+    const filePath = path.join(folder, file)
+    let stats
+
+    try {
+      stats = await fs.stat(filePath) // status información del archivo
+    } catch {
+      console.error(`No se ha podido leer el archivo ${folder}`)
+      process.exit(1)
     }
 
-    const filesPromises = files.map( async file =>{
-        const filePath = path.join(folder, file)
-        let stats
+    const isDirectory = stats.isDirectory()
+    const fileType = isDirectory ? 'd' : 'f'
+    const fileSize = stats.size.toString()
+    const fileModified = stats.mtime.toLocaleString()
 
-        try{
-            stats = await fs.stat(filePath) //status información del archivo
-        } catch{
-            console.error(`No se ha podido leer el archivo ${folder}`)
-            process.exit(1)
-        }
+    return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
+  })
 
-        const isDirectory = stats.isDirectory()
-        const fileType = isDirectory ? 'd' : 'f'
-        const fileSize = stats.size.toString()
-        const fileModified = stats.mtime.toLocaleString()
-
-        return `${pc.bgMagenta(fileType)} ${pc.blue(file.padEnd(20))} ${pc.green(fileSize.padStart(10))} ${pc.yellow(fileModified)}`
-
-    })
-    
-    const filesInfo = await Promise.all(filesPromises)
-    filesInfo.forEach(fileInfo => console.log(fileInfo))
+  const filesInfo = await Promise.all(filesPromises)
+  filesInfo.forEach(fileInfo => console.log(fileInfo))
 }
 
 ls(folder)
-
