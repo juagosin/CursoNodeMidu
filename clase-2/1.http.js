f50059ac335601c7e4d9f627bbd438301091656b
Juan <jugomez@howdeniberia.com>
2024-02-06 17:19:46 +0100

    First http create

---
+++ clase-2/1.http.js
@@ -0,0 +1,11 @@
+const http = require('node:http')
+
+const desiredPort = process.env.PORT ?? 1234
+const server = http.createServer((req, res) => {
+  console.log('request received: ', req.url)
+  res.end('Hola mundo')
+})
+
+server.listen(desiredPort, () => {
+  console.log(`servidor escuchando en el puerto http://localhost:${desiredPort}`)
+})
