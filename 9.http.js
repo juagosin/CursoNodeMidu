9e80cbfee730beea1e7f2c78d63950a2dcba1682
Juan <jugomez@howdeniberia.com>
2024-02-06 11:11:20 +0100

    Last mod, desired porrt

--- 9.http.js
+++ 9.http.js
@@ -1,11 +1,13 @@
 const http = require('node:http')
 const { findAvailablePort } = require('./10.free-port')
+
+const desiredPort = process.env.PORT ?? 3000
 const server = http.createServer((req, res) => {
   console.log('request received')
   res.end('Hola mundo')
 })
 
-findAvailablePort(1234).then(port => {
+findAvailablePort(desiredPort).then(port => {
   server.listen(0, () => {
     console.log(`servidor escuchando en el puerto http://localhost:${server.address().port}`)
   })
