083d7afc828fafd9a50176dd95b42470dc349c91
Juan <jugomez@howdeniberia.com>
2024-02-19 17:35:31 +0100

    Use REST CLient extension

---
+++ clase-2/2.routing.js
@@ -0,0 +1,32 @@
+const http = require('node:http')
+// common JS->modules
+const dittoJSON = require('./pokemon/ditto.json')
+
+const processRequest = (req, res) => {
+  const { method, url } = req
+  switch (method) {
+    case 'GET':
+      switch (url) {
+        case '/pokemon/ditto':
+          res.setHeader('Content-Type', 'application/json; charset=utf-8')
+          return res.end(JSON.stringify(dittoJSON))
+        default:
+          res.statusCode = 404
+          res.setHeader('Content-Type', 'text/html; charset=utf-9')
+          return res.end('<h1>404</h1>')
+      }
+    case 'POST':
+      switch (url) {
+        case '/pokemon':
+          const body = ''
+
+          // ??
+      }
+  }
+}
+
+const server = http.createServer(processRequest)
+
+server.listen(1234, () => {
+  console.log('server listening on port http://localhost:1234')
+})
