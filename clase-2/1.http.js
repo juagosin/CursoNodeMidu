281242e56253da524d1943c1319c399b00a62295
Juan <jugomez@howdeniberia.com>
2024-02-19 16:10:59 +0100

    Creating other routes

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -3,10 +3,16 @@ const http = require('node:http')
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
+  res.setHeader('Content-Type', 'text/html; charset=utf-8')
   if (req.url === '/') {
-    res.statusCode = 200
-    res.setHeader('Content-Type', 'text/html; charset=utf-8')
-    res.end('Bienvenido a mi página de inicio')
+    res.statusCode = 200 // OK
+    res.end('<h1>Bienvenido a mi página de inicio</h1>')
+  } else if (req.url === '/contacto') {
+    res.statusCode = 200 // OK
+    res.end('<h1>Contacto</h1>')
+  } else {
+    res.statusCode = 404 // Not found
+    res.end('<h1>404</h1>')
   }
 }
 
