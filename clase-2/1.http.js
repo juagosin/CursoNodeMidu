b0359d70a4eed5f4b7d18e5d8d43c8e3d2e3238e
Juan <jugomez@howdeniberia.com>
2024-02-19 15:55:55 +0100

    Refactoring

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,10 +1,13 @@
 const http = require('node:http')
 
 const desiredPort = process.env.PORT ?? 1234
-const server = http.createServer((req, res) => {
+
+const processRequest = (req, res) => {
   console.log('request received: ', req.url)
   res.end('Hola mundo')
-})
+}
+
+const server = http.createServer(processRequest)
 
 server.listen(desiredPort, () => {
   console.log(`servidor escuchando en el puerto http://localhost:${desiredPort}`)
