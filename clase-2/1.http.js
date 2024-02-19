d619ef3a28ac02813bdb1ff0f9ea1ad269b95920
Juan <jugomez@howdeniberia.com>
2024-02-19 16:41:39 +0100

    Buffer image

--- clase-2/1.http.js
+++ clase-2/1.http.js
@@ -1,12 +1,22 @@
 const http = require('node:http')
+const fs = require('node:fs')
 
 const desiredPort = process.env.PORT ?? 1234
 
 const processRequest = (req, res) => {
   res.setHeader('Content-Type', 'text/html; charset=utf-8')
   if (req.url === '/') {
-    res.statusCode = 200 // OK
     res.end('<h1>Mi página</h1>')
+  } else if (req.url === '/imagen-super-bonita.png') {
+    fs.readFile('./foto.png', (err, data) => {
+      if (err) {
+        res.statusCode = 500
+        res.end('<h1>500 Internarl Server Error</h1>')
+      } else {
+        res.setHeader('Content-Type', 'image/png')
+        res.end(data)
+      }
+    })
   } else if (req.url === '/contacto') {
     res.statusCode = 200 // OK
     res.end('<h1>Contacto</h1>')
