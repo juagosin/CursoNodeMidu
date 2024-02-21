0185a6135d26b2abd9e0507e771446d1dda2303c
Juan <jugomez@howdeniberia.com>
2024-02-21 16:05:02 +0100

    Create 404 Error

--- clase-2/3.express.js
+++ clase-2/3.express.js
@@ -1,10 +1,9 @@
 const express = require('express')
-const app = express()
 const ditto = require('./pokemon/ditto.json')
-
-app.disable('x-powered-by')
-
 const PORT = process.env.PORT ?? 1234
+
+const app = express()
+app.disable('x-powered-by')
 
 app.get('/pokemon/ditto', (req, res) => {
   res.json(ditto)
@@ -22,6 +21,10 @@ app.post('/pokemon', (req, res) => {
     data.timestamp = Date.now()
     res.status(201).json(data)
   })
+})
+
+app.use((req, res) => {
+  res.status(404).send('<h1>404</h1>')
 })
 
 app.listen(PORT, () => {
