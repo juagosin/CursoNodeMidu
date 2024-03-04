63fcdd44fc39fbc635c3c3f2a27cba97c54f1a11
Juan <jugomez@howdeniberia.com>
2024-03-04 16:26:53 +0100

    POST movie without validation

--- clase-3/app.js
+++ clase-3/app.js
@@ -1,7 +1,9 @@
 const express = require('express') // require -->CommonJS
+const crypto = require('node:crypto')
 const movies = require('./movies.json')
 
 const app = express()
+app.use(express.json())
 app.disable('x-powered-by') // Deshabilita cabecera x-powered-by
 
 app.get('/movies', (req, res) => {
@@ -20,6 +22,32 @@ app.get('/movies/:id', (req, res) => { // path-to-regexp
   const movie = movies.find(movie => movie.id === id)
   if (movie) return res.json(movie)
   res.status(404).json({ message: 'Movie not found' })
+})
+
+app.post('/movies', (req, res) => {
+  const {
+    title,
+    genre,
+    year,
+    director,
+    duration,
+    rate,
+    poster
+  } = req.body
+  const newMovie = {
+    id: crypto.randomUUID(),
+    title,
+    genre,
+    director,
+    year,
+    duration,
+    rate: rate ?? 0,
+    poster
+  }
+  // Esto no sería REST, porque estamos guardando el estado de la apliación en memoria
+  movies.push(newMovie)
+
+  res.status(201).json(newMovie)
 })
 
 const PORT = process.env.PORT ?? 1234
