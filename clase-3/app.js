5d3d944b82d35b386bc482173cc49fdb9cd017c5
Juan <jugomez@howdeniberia.com>
2024-03-04 15:59:16 +0100

    GET 1 id, query param

--- clase-3/app.js
+++ clase-3/app.js
@@ -4,12 +4,15 @@ const movies = require('./movies.json')
 const app = express()
 app.disable('x-powered-by') // Deshabilita cabecera x-powered-by
 
-app.get('/', (req, res) => {
-  res.json({ message: 'hola mundo' })
-})
-
 app.get('/movies', (req, res) => {
   res.json(movies)
+})
+
+app.get('/movies/:id', (req, res) => { // path-to-regexp
+  const { id } = req.params
+  const movie = movies.find(movie => movie.id === id)
+  if (movie) return res.json(movie)
+  res.status(404).json({ message: 'Movie not found' })
 })
 
 const PORT = process.env.PORT ?? 1234
