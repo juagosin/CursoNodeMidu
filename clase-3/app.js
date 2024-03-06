6769cdf0e3407ee35d5b029f20a597791a09bccc
Juan <jugomez@howdeniberia.com>
2024-03-06 15:56:24 +0100

    DELETE Method and CORS solution

--- clase-3/app.js
+++ clase-3/app.js
@@ -7,7 +7,19 @@ const app = express()
 app.use(express.json())
 app.disable('x-powered-by') // Deshabilita cabecera x-powered-by
 
+// métodos normales: GET/HEAD/POST
+// métodos complejos: PUT/PATCH/DELETE
+
+const ACCEPTED_ORIGINS = [
+  'http://localhost:8080',
+  'http://localhost:1234',
+  'http://movies.com'
+]
 app.get('/movies', (req, res) => {
+  const origin = req.header('origin')
+  // si la peticion es del mismo ORIGIN, no se envía la cabecera origin
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) { res.header('Access-Control-Allow-Origin', 'http://localhost:8080') }
+
   const { genre } = req.query
   if (genre) {
     const filteredMovies = movies.filter(
@@ -41,7 +53,20 @@ app.post('/movies', (req, res) => {
   res.status(201).json(newMovie)
 })
 
+app.delete('/movies/:id', (req, res) => {
+  const { id } = req.params
+  const movieIndex = movies.findIndex(movie => movie.id === id)
+  if (movieIndex === -1) {
+    return res.status(404).json({ message: 'Movie not found' })
+  }
+  movies.splice(movieIndex, 1)
+  return res.json({ message: 'Movie deleted' })
+})
+
 app.patch('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  // si la peticion es del mismo ORIGIN, no se envía la cabecera origin
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) { res.header('Access-Control-Allow-Origin', 'http://localhost:8080') }
   const result = validatePartialMovie(req.body)
   if (!result.success) {
     return res.status(400).json({ error: JSON.parse(result.error.message) })
@@ -61,6 +86,16 @@ app.patch('/movies/:id', (req, res) => {
   movies[movieIndex] = updateMovie
 
   return res.json(updateMovie)
+})
+
+app.options('/movies/:id', (req, res) => {
+  const origin = req.header('origin')
+  // si la peticion es del mismo ORIGIN, no se envía la cabecera origin
+  if (ACCEPTED_ORIGINS.includes(origin) || !origin) {
+    res.header('Access-Control-Allow-Origin', 'http://localhost:8080')
+    res.header('Access-Control-Allow-Method', 'GET, POST, PATCH, DELETE')
+  }
+  res.send()
 })
 
 const PORT = process.env.PORT ?? 1234
